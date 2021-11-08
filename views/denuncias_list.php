e8f0c248a51a6896417610e392dd1236d230563e
bismarnay81 <93796180+bismarnay81@users.noreply.github.com>
2021-11-08 19:08:03 -0400

    actualizacion7

--- views/denuncias_list.php
+++ views/denuncias_list.php
@@ -3,11 +3,12 @@ function denuncias_list() {
     ?>
     
     <link type="text/css" rel="stylesheet" href="<?php echo WP_PLUGIN_URL; ?>/crud-main/resources/css/materialize.min.css"  media="screen,projection"/>
-    <div class="wrap">
+    <div id="cuadro1" class="wrap">
         <h2>Central de riesgo</h2>
         <div class="tablenav top">
             <div class="alignleft actions">
                 <button type='button' class="btn-floating btn waves-effect waves-light red" onclick="location.href='<?php echo admin_url('admin.php?page=denuncias-nuevo'); ?>';"><i class="material-icons">add</i></button>
+                <button type='button' class="btn-floating btn waves-effect waves-light red" onclick="vercuadro()"><i class="material-icons">add</i></button>
             </div>
            
         </div>
@@ -45,7 +46,35 @@ function denuncias_list() {
             <?php } ?>
         </table>
     </div>
+    <div id="cuadro2" class="wrap">
+        <h2>cuadro2</h2>
+        <button type='button' class="btn-floating btn-large waves-effect waves-light blue" onclick="volver()"><i class="material-icons">reply</i></button>
+    </div>    
         <link href="<?php echo WP_PLUGIN_URL; ?>/crud-main/resources/icon.css" rel="stylesheet">
         <script type="text/javascript" src="<?php echo WP_PLUGIN_URL; ?>/crud-main/resources/js/materialize.min.js"></script>
+        <script>		
+           
+            jQuery(document).ready(function($){
+                // now you can use jQuery code here with $ shortcut formatting
+                // this will execute after the document is fully loaded
+                // anything that interacts with your html should go here
+                $("#cuadro2").slideUp("slow");
+                $("#cuadro1").slideDown("slow");
+            }); 
+           function vercuadro(){  
+                
+                //$("#cuadro1").slideUp("slow");
+                //$("#cuadro2").slideUp("slow");
+                jQuery("#cuadro1").removeClass('slided').slideUp('fast');
+                jQuery("#cuadro2").removeClass('slided').slideDown('fast'); 
+            }
+            function volver(){  
+                
+                //$("#cuadro1").slideUp("slow");
+                //$("#cuadro2").slideUp("slow");
+                jQuery("#cuadro2").removeClass('slided').slideUp('fast');
+                jQuery("#cuadro1").removeClass('slided').slideDown('fast'); 
+            }
+        </script>    
     <?php
 }

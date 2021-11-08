85eb79ec08289aa025ecc93a558899ec05ce15d0
bismarnay81 <93796180+bismarnay81@users.noreply.github.com>
2021-11-08 18:07:19 -0400

    actualizacion6

--- views/denuncias_list.php
+++ views/denuncias_list.php
@@ -1,15 +1,24 @@
 <?php
 function denuncias_list() {
     ?>
-    <link type="text/css" href="<?php echo WP_PLUGIN_URL; ?>/iby-master/css/style-admin.css" rel="stylesheet" />
+    
+    <link type="text/css" rel="stylesheet" href="<?php echo WP_PLUGIN_URL; ?>/crud-main/resources/css/materialize.min.css"  media="screen,projection"/>
     <div class="wrap">
         <h2>Central de riesgo</h2>
         <div class="tablenav top">
             <div class="alignleft actions">
                 <button type='button' class="btn-floating btn waves-effect waves-light red" onclick="location.href='<?php echo admin_url('admin.php?page=denuncias-nuevo'); ?>';"><i class="material-icons">add</i></button>
             </div>
-            <br class="clear">
+           
         </div>
+        <hr>
+        <div class="row">
+                <div class="input-field col s6">  
+                 
+                  <i class="material-icons">search</i>
+                  <input name="buscar_denuncia" value="<?php echo $buscar_denuncia; ?>" type="text"  placeholder="Escribe para buscar...">
+                </div> 
+        </div>        
         <?php
             $args = array(
                 'post_type'        => 'pos_denuncias',
@@ -36,9 +45,7 @@ function denuncias_list() {
             <?php } ?>
         </table>
     </div>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <!-- Compiled and minified JavaScript -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+        <link href="<?php echo WP_PLUGIN_URL; ?>/crud-main/resources/icon.css" rel="stylesheet">
+        <script type="text/javascript" src="<?php echo WP_PLUGIN_URL; ?>/crud-main/resources/js/materialize.min.js"></script>
     <?php
 }
